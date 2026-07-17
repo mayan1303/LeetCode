c1ec1ec133fea571e347c1958fd73280e315ad15
mayan1303 <jindalmayan1303@gmail.com>
2026-07-17 11:03:42 +0530

    Time: 3 ms (17.70%) | Memory: 8.5 MB (50.03%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0){
-            return 0;
-        }
-        int m;
-        for(int i=1;i<x;i++){
-            if(i > x/i){
-                m=i-1;
-                break;
+        int l=1,h=x;
+        int ans=0;
+
+        while(l<=h){
+            int mid=l+ (h-l) /2;
+            if(mid<= x/mid){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                h= mid-1;
             }
         }
-        return m;
+        return ans;
     }
 };
