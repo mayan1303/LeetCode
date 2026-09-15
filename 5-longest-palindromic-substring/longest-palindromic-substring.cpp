6d3211b2d31ecca1c7fa119725e73a98bd4255d6
mayan1303 <jindalmayan1303@gmail.com>
2026-09-15 17:26:23 +0530

    Time: 7 ms (84.99%) | Memory: 9.2 MB (91.84%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start=0;
+        int len=0;
+
+        for(int i=0;i<s.size();i++){
+            int l=i;
+            int r=i;
+            //odd length palindrome
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>len){
+                    len=r-l+1;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+
+            //even length palindrome
+            l=i;
+            r=i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>len){
+                    len=r-l+1;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start,len);
+    }
+};
